991409d9ee8f00db8ac77964910e1eeec71afa19
PagiY <pagiyap003@gmail.com>
2022-09-21 08:01:14 +0800

    Done "Index Page" and "Search" specifications
    - included <a> tag in index.html with href pointing to wiki/<title>
    - added search view which searches through all wiki entries and shows
      the wiki entry if it matches, or returns a list of all entries with a
      substring of the searched entry

--- Project-01/encyclopedia/views.py
+++ Project-01/encyclopedia/views.py
@@ -10,6 +10,11 @@ def index(request):
     
 def wiki(request, title):
     
+    """
+    Wiki view for showing the wiki page if available.
+    Shows error page if wiki page is not available.
+    """
+    
     entry = util.get_entry(title)
     
     if entry is None: 
@@ -18,5 +23,38 @@ def wiki(request, title):
     return render(request, "encyclopedia/wiki.html",{
         "title": title.capitalize(),
         "content": entry
+    })
+    
+def search(request):
+    
+    """
+    Search view for searching through wiki entry.
+    Displays the wiki entry of the searched entry.
+    If searched entry is not found, check if a substring of the searched entry exists 
+    in the list of all the entries, and display the list to the user.
+    """
+    
+    searched = request.GET.get('q')
+    entry = util.get_entry(searched)
+    
+    if entry is None:
+        substr_entries = []
+        entries = util.list_entries()
+        for ent in entries:
+            if searched in ent.lower():
+                substr_entries.append(ent)
+            
+        if len(substr_entries) == 0:
+            return render(request, "encyclopedia/error.html")
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": substr_entries
+            })
+            
+    title = searched.lower()
+    
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title.capitalize(),
+        "content": entry    
     })
 
