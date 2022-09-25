46cda03d8d85bf8bea168e04760372b39adeff81
PagiY <pagiyap003@gmail.com>
2022-09-25 20:24:00 +0800

    Updated models and forms

--- Project-02/auctions/forms.py
+++ Project-02/auctions/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from auctions.models import List, CATEGORIES
+from auctions.models import List, Bid, CATEGORIES
 
 class ListForm(forms.ModelForm):
     class Meta:
@@ -11,5 +11,13 @@ class ListForm(forms.ModelForm):
             'img_url'           : forms.TextInput(attrs = {'class': 'form-control'}),
             'starting_price'    : forms.NumberInput(attrs = {'class': 'form-control'}),
             'category'          : forms.Select(choices = CATEGORIES, attrs = {'class': 'form-control'})   
+        }
+        
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid 
+        fields = ['price']
+        widgets = {
+            'price'             : forms.TextInput(attrs={'class': 'form-control'})
         }
             
--- Project-02/auctions/models.py
+++ Project-02/auctions/models.py
@@ -8,16 +8,16 @@ CATEGORIES = [ ('entertainment', 'Entertainment'),
                ('grocery', 'Grocery')
 ]
 
-class User(AbstractUser):
+class User(AbstractUser): 
     pass
 
 class List(models.Model):
     title           = models.CharField(max_length = 64)
     description     = models.CharField(max_length = 1000)
     img_url         = models.CharField(max_length = 128, default = '')
-    starting_price  = models.FloatField()
+    starting_price  = models.DecimalField(decimal_places = 5, max_digits = 15)
     category        = models.CharField(max_length = 64, choices = CATEGORIES)
-    user            = models.ForeignKey(User, on_delete = models.CASCADE)
+    user            = models.ForeignKey(User, on_delete = models.CASCADE) 
     status          = models.BooleanField(default = True)
     
     def __str__(self):
@@ -25,8 +25,16 @@ class List(models.Model):
     
 class Bid(models.Model):
     listing         = models.ForeignKey(List, on_delete = models.CASCADE)
+    user            = models.ForeignKey(User, on_delete = models.CASCADE, default = None)
+    price           = models.DecimalField(decimal_places = 5, max_digits = 15, default = 0.00)
+    
+    def __str__(self):
+        return f"{self.listing} {self.price} {self.user}" 
     
 class Comment(models.Model):
     listing         = models.ForeignKey(List, on_delete = models.CASCADE)
-    user            = models.ForeignKey(User, on_delete = models.CASCADE)
+    user            = models.ForeignKey(User, on_delete = models.CASCADE, default = None)
     user_comment    = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return f"{self.listing} {self.user} {self.user_comment}"
