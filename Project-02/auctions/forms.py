cd30daf15f47e789af28f1bf45aad95fefde9215
PagiY <pagiyap003@gmail.com>
2022-10-06 15:10:11 +0800

    Added form for comment

--- Project-02/auctions/forms.py
+++ Project-02/auctions/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from auctions.models import List, Bid, CATEGORIES
+from auctions.models import List, Bid, Comment, CATEGORIES
 
 class ListForm(forms.ModelForm):
     class Meta:
@@ -23,4 +23,14 @@ class BidForm(forms.ModelForm):
         
 class CategoriesForm(forms.Form):
     category = forms.ChoiceField(choices = CATEGORIES, widget=forms.Select(attrs = {'class': 'form-control'}))
-            
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['user_comment']
+        labels = {
+            'user_comment' : 'Comments'
+        }
+        widgets = {
+            'user_comment'             : forms.TextInput(attrs={'class': 'form-control'})
+        }  
