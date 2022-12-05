be50e30ce50248f5277644e3f2dccc45d5f0af0b
PagiY <pagiyap003@gmail.com>
2022-12-05 22:09:32 +0800

    added POST request when sending email

--- Project-03/mail/static/mail/inbox.js
+++ Project-03/mail/static/mail/inbox.js
@@ -20,6 +20,30 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#send-email').onClick = (e) => {
+    
+    e.preventDefault();
+
+    let recipients = document.querySelector('#compose-recipients').value;
+    let subject = document.querySelector('#compose-subject').value;
+    let body = document.querySelector('#compose-body').value;
+
+    fetch('/emails', {
+      method: "POST",
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+    });
+  }
+
 }
 
 function load_mailbox(mailbox) {
@@ -31,4 +55,5 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  
 }
