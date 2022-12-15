0c254c70630fb58defacd5b51fa258cc870d1b88
PagiY <pagiyap003@gmail.com>
2022-12-15 16:42:52 +0800

    Updated urls: simplified

--- Project-04/network/urls.py
+++ Project-04/network/urls.py
@@ -8,13 +8,7 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    
     path("post", views.post, name = "post"),
-    path("like/<int:post_id>", views.like_post, name = "like_post"),
-    path("unlike/<int:post_id>", views.unlike_post, name = "unlike_post"),
-    path("profile/<int:user_id>", views.show_profile, name="show_profile"),
-    path("follow/<int:user_id>", views.follow, name = "follow"),
-    path("unfollow/<int:user_id>", views.unfollow, name = "unfollow"),
-    path("following", views.following, name = "following"),
-    
+    path("profile/<int:id>", views.profile, name = "profile"),
+    path("following", views.following, name = "following")
 ]
